2e502f5ee043f6894fac8c8c05b82ff6bc26da76
Filip <filip.hrkac@diplomat.ba>
2023-11-10 16:22:20 +0100

    adding footer, removing console log

--- components/UI/Layout/Layout.js
+++ components/UI/Layout/Layout.js
@@ -3,12 +3,12 @@ import MainNavigation from "../../MainNavigation/MainNavigation";
 import { useSession } from "next-auth/react";
 import useStore from "../../../store/useStore";
 import { useEffect } from "react";
-
+import classes from "./Layout.module.css";
 const Layout = (props) => {
   const { data: session, status } = useSession();
   const setSession = useStore((state) => state?.setSession);
   const router = useRouter();
-  const sessionn = useStore((state) => state.session);
+  const version = require("../../../package.json").version;
   useEffect(() => {
     if (session) {
       setSession(session);
@@ -23,6 +23,12 @@ const Layout = (props) => {
     <div>
       {session && <MainNavigation sessionName={session.user.name} />}
       {props.children}
+      <footer className={classes.footer}>
+        <span className={classes.footerTxt}>
+          PayTracker © All rights reserved
+        </span>
+        <span className={classes.version}>v{version}</span>
+      </footer>
     </div>
   );
 };
--- pages/auth/index.js
+++ pages/auth/index.js
@@ -17,7 +17,6 @@ const Authentication = (props) => {
       },
     });
     setIsLoading(false);
-    console.log(response);
     if (response.status === 200) {
       setErrorMsg("");
       window.location.href = "/auth";
