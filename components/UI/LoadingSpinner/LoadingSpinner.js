5a75e819df94825ffc3493cfc3b02442dfbc33f3
Filip <filip.hrkac@diplomat.ba>
2023-11-20 09:42:27 +0100

    loading and more

--- components/UI/LoadingSpinner/LoadingSpinner.js
+++ components/UI/LoadingSpinner/LoadingSpinner.js
@@ -1,10 +1,15 @@
 import classes from "./LoadingSpinner.module.css";
 
-const LoadingSpinner = ({ width, height }) => {
+const LoadingSpinner = ({ width, height, lineWidth }) => {
   return (
     <div
       className={classes.loaderItem}
-      style={{ width: `${width}px`, height: `${height}px` }}
+      style={{
+        width: `${width}px`,
+        height: `${height}px`,
+        border: `${lineWidth}px solid rgb(200, 204, 204)`,
+        borderTop: `${lineWidth}px solid rgb(64, 64, 250)`,
+      }}
     ></div>
   );
 };
