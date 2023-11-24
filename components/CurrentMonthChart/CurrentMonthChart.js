def9d523bf1350270943d943157d524f5fd3bf33
Filip <filip.hrkac@diplomat.ba>
2023-11-24 10:16:35 +0100

    opacity to containers

--- components/CurrentMonthChart/CurrentMonthChart.js
+++ components/CurrentMonthChart/CurrentMonthChart.js
@@ -1,7 +1,33 @@
+import { useEffect, useRef, useState } from "react";
 import { backgroundColorArray, borderColorArray } from "../../lib/chart-bgs";
 import BarChart from "../BarChart/BarChart";
 import classes from "./CurrentMonthChart.module.css";
 const CurrentMonthChart = ({ expenses, date }) => {
+  const containerRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    const checkContentVisibility = (element) => {
+      const rect = element.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <=
+          (window.innerWidth || document.documentElement.clientWidth)
+      );
+    };
+    window.addEventListener("scroll", () => {
+      const isInViewport = checkContentVisibility(containerRef.current);
+      if (isInViewport) {
+        setIsVisible(true);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", null);
+    };
+  }, []);
+
   const toDate = new Date(date);
   const filteredExpensesByCurrMonth = expenses.filter(
     (expense) => new Date(expense.date).getMonth() === toDate.getMonth()
@@ -51,7 +77,12 @@ const CurrentMonthChart = ({ expenses, date }) => {
     };
   }
   return (
-    <div className={classes.currentMonthChart}>
+    <div
+      className={`${classes.currentMonthChart} ${
+        isVisible ? classes.show : ""
+      }`}
+      ref={containerRef}
+    >
       {thisMonthChartsData && thisMonthChartsData.labels ? (
         <BarChart chartData={thisMonthChartsData} />
       ) : null}
